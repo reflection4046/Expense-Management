4d993bd664acfc52300a85942a2db7a9f1a342e2
Nahidul Isalm <nahidulaiub@gmail.com>
2023-08-07 21:24:41 +0600

    project done

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,13 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import { Form, Input, message } from "antd";
-import { Link,useNavigate } from 'react-router-dom';
-import axios from 'axios'
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import Spinner from "../components/Spinner";
+import "../styles/RegisterPage.css";
 const Register = () => {
-  const navigate =useNavigate();
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-    //from submit
+  //from submit
   const submitHandler = async (values) => {
     try {
       setLoading(true);
@@ -20,36 +21,39 @@ const Register = () => {
     }
   };
 
-   //prevent for login user
-   useEffect(() => {
+  //prevent for login user
+  useEffect(() => {
     if (localStorage.getItem("user")) {
       navigate("/");
     }
   }, [navigate]);
-
   return (
     <>
-    <div className="register-page ">
+      <div className="register-page ">
         {loading && <Spinner />}
-        <Form layout="vertical" onFinish={submitHandler}>
-          <h1>Register Form</h1>
+        <Form
+          className="register-form"
+          layout="vertical"
+          onFinish={submitHandler}
+        >
+          <h2>Register Form</h2>
           <Form.Item label="Name" name="name">
-            <Input />
+            <Input type="text" required />
           </Form.Item>
           <Form.Item label="Email" name="email">
-            <Input type="email" />
+            <Input type="email" required />
           </Form.Item>
           <Form.Item label="Password" name="password">
-            <Input type="password" />
+            <Input type="password" required />
           </Form.Item>
           <div className="d-flex justify-content-between">
-            <Link to="/login">Already Register ? Check Here to login</Link>
-            <button className="btn btn-primary">Register</button>
+            <Link to="/login">Already Register? login here!</Link>
+            <button className="btn ">Register</button>
           </div>
         </Form>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
